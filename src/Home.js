20e945e7ad978b544a69c1fa2d1f869d86b38ba6
gomez50057 <gomez.50057@gmail.com>
2025-02-05 12:32:52 -0600

    add gsap

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,32 @@
 // src/Home.js
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './styles.css';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Home = () => {
+  const logoImgRef = useRef(null);
+
+  useEffect(() => {
+
+    gsap.to(logoImgRef.current, {
+      x: "-35vw",
+      y: "85vh",
+      scale: 0.45,
+      duration: 4,
+      ease: "power4.inOut",
+      scrollTrigger: {
+        trigger: logoImgRef.current, // Elemento que dispara la animación
+        start: 'Top 2%',          // Cuando el tope del elemento alcanza el centro del viewport
+        end: '+=1000',                // La cantidad de scroll (en píxeles) que dura la animación (puedes ajustar este valor)
+        scrub: true,                 // Sincroniza la animación con el scroll, haciendo que se reproduzca y se revierta al hacer scroll hacia arriba
+        markers: true,               // Muestra marcadores para depuración (quita en producción)
+      }
+    });
+  }, []);
+
   return (
     <section className="home">
       <div className="logotipo">
@@ -14,15 +38,13 @@ const Home = () => {
           <img src={`${process.env.PUBLIC_URL}/img/nombre.png`} alt="Logo de Biblioteca Digital de Planeación" />
         </div>
 
-        <div className="homeInfe">
-          <img src={`${process.env.PUBLIC_URL}/img/homeInferior.png`} alt="Logo de Biblioteca Digital de Planeación" />
-        </div>
-
-        <div className="logoImg">
+        <div className="logoImg" ref={logoImgRef}>
           <img src={`${process.env.PUBLIC_URL}/img/logoImg.png`} alt="Logo de Biblioteca Digital de Planeación" />
         </div>
 
-        
+        <div className="homeInfe">
+          <img src={`${process.env.PUBLIC_URL}/img/homeInferior.png`} alt="Logo de Biblioteca Digital de Planeación" />
+        </div>
       </div>
     </section>
   );
